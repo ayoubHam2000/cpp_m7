58cf2eba25887e1c1a4185ab179e3a2f86687c8f
Ayoub Ben hamou <aben-ham@e2r4p5.1337.ma>
2022-09-24 10:54:56 +0100

    v2

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,5 +1,57 @@
 #include <iostream>
 #include "Array.hpp"
+#define MAX_VAL 750
+
+
+int	intra_test()
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+	return (0);
+}
 
 void	test1()
 {
@@ -38,6 +90,7 @@ void	test1()
 int	main()
 {
 	test1();
+	intra_test();
 	// system("leaks template");
 	return (0);
 }
